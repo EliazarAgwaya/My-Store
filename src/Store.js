9f10115a88918e7efec3d09208d157d22bce328a
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-09-21 08:47:03 +0300

    uses Promise.all to fetch the data from the APIs

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Users from "./Users";
 import Orders from "./Orders";
 import Products from "./Products";
 
 function Store () {
+    const [users, setUsers] = useState([])
+    const [products, setProducts] = useState([])
+    const [orders, setOrders] = useState([])
+
+    const urls = [
+        "https://assessment.api.vweb.app/users",
+        "https://assessment.api.vweb.app/products",
+        "https://assessment.api.vweb.app/orders",
+      ];
+
+    useEffect(() => {
+        Promise.all(urls.map((url) => fetch(url)
+            .then((res) =>res.json())))
+            .then(([users, products, orders]) => {
+                setUsers(users);
+                setProducts(products);
+                setOrders(orders);
+            })
+    }, [])
+    console.log(orders)
     return (
         <div>
             <Users/>
